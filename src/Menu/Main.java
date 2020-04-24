2216c49f1944558b706d2bf0bff6c251f3cdc524
Grisha <grigorygrisha9@gmail.com>
2020-04-24 21:55:42 +0300

    3

---
+++ src/Menu/Main.java
@@ -0,0 +1,9 @@
+package Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu mainMenu = new MainMenu();
+        mainMenu.printMenu();
+        mainMenu.interactWithMenu();
+    }
+}
---
+++ src/Menu/MainMenu.java
@@ -0,0 +1,20 @@
+package Menu;
+
+public class MainMenu extends AbstractMenu {
+
+    public MainMenu() {
+        super();
+        initMenuItems();
+    }
+
+    private void initMenuItems() {
+        this.items.add("Create");
+        this.items.add("Search");
+        this.items.add("Sort");
+    }
+
+    @Override
+    public void interactWithMenu() {
+//TODO: Обработка ввода и вызов метода контроллера
+    }
+}
